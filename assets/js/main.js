996d0def9ea0fc0169eb147d1ea25346f28e468a
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-10-23 15:30:13 +0200

    Javascript

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+let result =(document.getElementById('h2-summ'));
+let num=0;
+
+ function button1(){
+    num++
+    result.innerHTML = num ;
+
+}
+function button2(){
+    num--
+    result.innerHTML = num ;
+
+}
+function button3(){
+    num= num +10
+    result.innerHTML = num ;
+
+}
+function button4(){
+    num= num -10
+    result.innerHTML = num ;
+
+}
+function button5(){
+    num= num +100
+    result.innerHTML = num ;
+
+}
+function button6(){
+    num= num -100
+    result.innerHTML = num ;
+
+}
+function button7(){
+    num= 0
+    result.innerHTML = num ;
+
+}
+function button8(){
+    num= num *2
+    result.innerHTML = num ;
+
+}
